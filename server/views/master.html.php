608c3082f82f263254b9ce971f09d56c3de1c62c
DalatCoder <hieuntctk42@gmail.com>
2021-11-25 06:58:24 +0700

    Change dashboard

--- server/views/master.html.php
+++ server/views/master.html.php
@@ -19,13 +19,26 @@
 <body>
 <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
     <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="/">SSF - Save For Future</a>
-    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse"
+            data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false"
+            aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="navbar-nav">
-        <div class="nav-item text-nowrap">
-            <a class="nav-link px-3" href="#">Đăng xuất</a>
-        </div>
+        <?php if ($is_logged_in): ?>
+            <div class="nav-item text-nowrap">
+                <a class="nav-link px-3" href="#">Đăng xuất</a>
+            </div>
+        <?php else: ?>
+            <div class="d-sm-flex">
+                <div class="nav-item text-nowrap">
+                    <a class="nav-link px-3" href="#">Tạo tài khoản</a>
+                </div>
+                <div class="nav-item text-nowrap">
+                    <a class="nav-link px-3" href="#">Đăng nhập</a>
+                </div>
+            </div>
+        <?php endif; ?>
     </div>
 </header>
 
